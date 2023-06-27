2b92f996b6f20c25e767ce13a48b7bcc0262ef9a
Peter Bjorklund <piot@hotmail.com>
2023-06-28 01:31:06 +0200

    fix: use generated cmake file

--- src/lib/bit_array.c
+++ src/lib/bit_array.c
@@ -7,7 +7,7 @@
 #include <imprint/tagged_allocator.h>
 
 /// Initializes a bit array
-/// @param self
+/// @param self bit array
 /// @param memory the allocator with no explicit free function.
 /// @param bitCount the maximum number of bits in the array
 void bitArrayInit(BitArray* self, struct ImprintAllocator* memory, size_t bitCount)
@@ -21,19 +21,23 @@ void bitArrayInit(BitArray* self, struct ImprintAllocator* memory, size_t bitCou
 }
 
 /// Resets the structure to its init state.
-/// @param self
+/// @param self  bit array
 void bitArrayDestroy(BitArray* self)
 {
-    //imprintMemoryFree(self->memory, self->array);
     self->array = 0;
 }
 
+/// Resets the bits
+/// @param self bit array
 void bitArrayReset(BitArray* self)
 {
+    tc_mem_clear_type_n(self->array, self->atomCount);
+    self->bitCount = 0;
+    self->numberOfBitsSet = 0;
 }
 
 /// checks if all bits are set
-/// @param self
+/// @param self  bit array
 /// @return true if all bits in the array are set
 bool bitArrayAreAllSet(const BitArray* self)
 {
@@ -41,8 +45,8 @@ bool bitArrayAreAllSet(const BitArray* self)
 }
 
 /// Finds the first bit that is not set in the array
-/// @param self
-/// @return
+/// @param self  bit array
+/// @return bit index in array
 size_t bitArrayFirstUnset(const BitArray* self)
 {
     for (size_t i = 0; i < self->atomCount; ++i) {
@@ -61,7 +65,7 @@ size_t bitArrayFirstUnset(const BitArray* self)
 }
 
 /// Sets the bit at the given index
-/// @param self
+/// @param self bit array
 /// @param index zero based index
 void bitArraySet(BitArray* self, size_t index)
 {
@@ -82,8 +86,8 @@ void bitArraySet(BitArray* self, size_t index)
 }
 
 /// Returrns the atom value that is located at the specified index.
-/// @param self
-/// @param fromIndex
+/// @param self bit array
+/// @param fromIndex starting the read from index
 /// @return the atom value at the specified index
 BitArrayAtom bitArrayGetAtomFrom(const BitArray* self, size_t fromIndex)
 {
@@ -95,35 +99,39 @@ BitArrayAtom bitArrayGetAtomFrom(const BitArray* self, size_t fromIndex)
         if (index >= self->bitCount) {
             continue;
         }
-        result |= bitArrayGet(self, index);
+        result |= (unsigned int)bitArrayGet(self, index);
     }
 
     return result;
 }
 
 /// Returns the bit value at the specified index
-/// @param self
-/// @param index
-/// @return
+/// @param self bit array
+/// @param index bit index to read from
+/// @return the read bit
 BitArrayBit bitArrayGet(const BitArray* self, size_t index)
 {
     size_t arrayIndex = index / BIT_ARRAY_BITS_IN_ATOM;
     size_t bitIndex = index % BIT_ARRAY_BITS_IN_ATOM;
 
     if (index >= self->bitCount) {
-        CLOG_ERROR("wrong");
+        CLOG_ERROR("wrong")
     }
 
     return (*(self->array + arrayIndex) >> bitIndex) & 0x1;
 }
 
 /// Returns the internal state of the BitArray. Not Implemented!
-/// @param self
+/// @param self bit array
 /// @param buf target buffer
 /// @param maxBuf maximum number of characters in the buf target buffer
-/// @return
+/// @return buf parameter
+/// @note not implemented yet
 const char* bitArrayToString(const BitArray* self, char* buf, size_t maxBuf)
 {
+    (void) self;
+    (void) maxBuf;
+
     buf[0] = 0;
     return buf;
 }
